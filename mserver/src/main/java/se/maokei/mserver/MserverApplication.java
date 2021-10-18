15f828725db189208524bac6f4d0afdd0fb243d8
maokei <rickard.joh@gmail.com>
2021-10-18 21:58:41 +0200

    Starters server and client

---
+++ mserver/src/main/java/se/maokei/mserver/MserverApplication.java
@@ -0,0 +1,13 @@
+package se.maokei.mserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MserverApplication.class, args);
+	}
+
+}
---
+++ mserver/src/test/java/se/maokei/mserver/MserverApplicationTests.java
@@ -0,0 +1,13 @@
+package se.maokei.mserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
