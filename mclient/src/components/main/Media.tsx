bc99afeca7b023adbaf7ec048d26091b0e161699
shirley_wang <shirleywang527@gmail.com>
2021-11-04 12:27:21 +0100

    lay ground of mediaList

--- mclient/src/components/main/Media.tsx
+++ mclient/src/components/main/Media.tsx
@@ -14,29 +14,18 @@ export const Media: React.FC<MediaProps> = ({
     mediaSubtitle,
 }) => {
     return (
-        <div className="card media mediaList-item">
-            <div className="card-content media-content">
-                <div className="media media-image-wrapper">
-                    <div className="media-left">
-                        <figure className="image is-48x48">
-                            <img src={imgSrc} alt={mediaTitle} />
-                        </figure>
-                    </div>
-                    <div className="media-content">
-                        <p className="title is-4">{mediaTitle}</p>
-                        <p className="subtitle is-6">{mediaSubtitle}</p>
-                    </div>
-                </div>
-
-                <div className="footer media-footer">
-                    <div className="card-footer-item media-footer-item">
-                        <button className="button is-dark">Like</button>
-                    </div>
-                    <div className="card-footer-item media-footer-item">
-                        <button className="button is-dark">Play</button>
-                    </div>
-                </div>
-            </div>
+        <div className="mediaList-item">
+            <figure className="image is-48x48">
+                <img src={imgSrc} alt={mediaTitle} />
+            </figure>
+            <p className="title is-4">{mediaTitle}</p>
+            <p className="subtitle is-6">{mediaSubtitle}</p>
+            <button className="button is-dark icon like-btn">
+                <i className="fas fa-heart"></i>
+            </button>
+            <button className="button is-dark icon play-btn">
+                <i className="fas fa-play"></i>
+            </button>
         </div>
     );
 };
