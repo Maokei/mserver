a667329f7765b049e051e4fdb4b8685de8d54e8f
maokei <rickard.joh@gmail.com>
2021-11-14 17:46:12 +0100

    OpenAPI doc for helloworld

--- mserver/src/main/java/se/maokei/mserver/controller/HelloWorld.java
+++ mserver/src/main/java/se/maokei/mserver/controller/HelloWorld.java
@@ -1,5 +1,7 @@
 package se.maokei.mserver.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -7,8 +9,11 @@ import reactor.core.publisher.Mono;
 import java.security.Principal;
 
 @RestController
+@Tag(name = "Test API", description = "API for testing purpose")
 public class HelloWorld {
+
     @GetMapping("/hello")
+    @Operation(description = "Hello world, returns a hello to logged in user")
     public Mono<String> greet(Mono<Principal> principal) {
         return principal
                 .map(Principal::getName)
