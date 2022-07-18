c001a38b8ef191c079a33ce3b7869de27921c536
maokei <rickard.joh@gmail.com>
2022-07-18 15:52:53 +0200

    Revised test setup Login

--- mclient/src/__tests__/Login.test.tsx
+++ mclient/src/__tests__/Login.test.tsx
@@ -1,7 +1,21 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import React from "react";
 
 import Login from "../components/auth/Login";
+
+const Wrapper = () => {
+	const [password, setPassword] = React.useState("");
+	const [username, setUsername] = React.useState("");
+	return (<Login
+		handleLoginSubmit={() => console.log("login")}
+		username={username}
+		password={password}
+		setUsername={setUsername}
+		setPassword={setPassword}
+		message={""}
+/>);
+}
 
 describe("Login", () => {
 	it("should render input element", () => {
@@ -10,19 +24,18 @@ describe("Login", () => {
 		expect(inputEl).toBeInTheDocument();
 	});
 
-	it("should be able to type into input", () => {
-		render(
-			<Login
-				handleLoginSubmit={() => console.log("login")}
-				username={""}
-				password={""}
-				setUsername={() => console.log("test1234")}
-				setPassword={() => console.log("12345")}
-				message={""}
-			/>
+ test("should be able to type into input", () => {
+		const { getByTestId, getByPlaceholderText } = render(
+			<Wrapper />
 		);
+		const testText = "123456";
 		const passwordInput = screen.getByTestId("password");
-		userEvent.type(passwordInput, "123456");
+
+		userEvent.type(passwordInput, testText);
+		//fireEvent.change(passwordInput, {target: {value: testText}})
+		waitFor(() => expect(passwordInput).toHaveValue(testText))
+
+		expect(passwordInput).toHaveDisplayValue(testText);
 	});
 
 	it("button should be enabled when username and password are filled", () => {
