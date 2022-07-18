de5dcde3e83b91dfb79fb3616e8e0d3e3867db58
shirley <shirleywang527@gmail.com>
2022-07-18 14:01:05 +0200

    modify tests

--- mclient/src/__tests__/Login.test.tsx
+++ mclient/src/__tests__/Login.test.tsx
@@ -3,50 +3,50 @@ import userEvent from "@testing-library/user-event";
 
 import Login from "../components/auth/Login";
 
-describe("input", () => {
-	it("accepts user typing", async () => {
+describe("Login", () => {
+	it("should render input element", () => {
 		render(<input type="text" data-testid="username" name="username" />);
-		const input = screen.getByTestId("username");
-		await userEvent.type(input, "Hello,{enter}World!");
+		const inputEl = screen.getByTestId("username");
+		expect(inputEl).toBeInTheDocument();
 	});
-});
 
-it("input value", () => {
-	render(
-		<Login
-			handleLoginSubmit={() => console.log("login")}
-			username={""}
-			password={""}
-			setUsername={() => console.log("test1234")}
-			setPassword={() => console.log("12345")}
-			message={""}
-		/>
-	);
-	const passwordInput = screen.getByTestId("password");
-	userEvent.type(passwordInput, "123456");
-});
+	it("should be able to type into input", () => {
+		render(
+			<Login
+				handleLoginSubmit={() => console.log("login")}
+				username={""}
+				password={""}
+				setUsername={() => console.log("test1234")}
+				setPassword={() => console.log("12345")}
+				message={""}
+			/>
+		);
+		const passwordInput = screen.getByTestId("password");
+		userEvent.type(passwordInput, "123456");
+	});
 
-it("button state in form", () => {
-	render(
-		<Login
-			handleLoginSubmit={() => console.log("login")}
-			username={""}
-			password={""}
-			setUsername={() => console.log("test1234")}
-			setPassword={() => console.log("12345")}
-			message={""}
-		/>
-	);
-	const button = screen.getByTestId("submitButton");
-	expect(button).toBeDisabled();
+	it("button should be enabled when username and password are filled", () => {
+		render(
+			<Login
+				handleLoginSubmit={() => console.log("login")}
+				username={""}
+				password={""}
+				setUsername={() => "test1234"}
+				setPassword={() => "12345"}
+				message={""}
+			/>
+		);
+		const button = screen.getByTestId("submitButton");
+		expect(button).toBeDisabled();
 
-	const usernameInput = screen.getByTestId("username");
-	const passwordInput = screen.getByTestId("password");
-	userEvent.type(usernameInput, "test1234");
-	userEvent.type(passwordInput, "123456");
-	expect(button).toBeEnabled();
+		const usernameInput = screen.getByTestId("username");
+		const passwordInput = screen.getByTestId("password");
+		userEvent.type(usernameInput, "test1234");
+		userEvent.type(passwordInput, "123456");
+		expect(button).toBeEnabled();
 
-	userEvent.clear(usernameInput);
-	userEvent.clear(passwordInput);
-	expect(button).toBeDisabled();
+		userEvent.clear(usernameInput);
+		userEvent.clear(passwordInput);
+		expect(button).toBeDisabled();
+	});
 });
