3f3afd92f6168928352ae2b33adbcc750a7f7152
shirley <shirleywang527@gmail.com>
2022-09-03 08:39:21 +0200

    commit changes

--- mclient/src/components/media/Media.tsx
+++ mclient/src/components/media/Media.tsx
@@ -1,104 +1,141 @@
 import { useState, useEffect, useRef } from "react";
 import styles from "./media.module.scss";
 import Player from "./player/Player";
-import dummyData from "../../dummyData.json";
 
 const Media = ({
-    baseUrl,
-    id,
-    title,
-    foreignId,
+	baseUrl,
+	id,
+	title,
+	foreignId,
 }: {
-    baseUrl: string;
-    id: string;
-    title: string;
-    foreignId: string;
+	baseUrl: string;
+	id: string;
+	title: string;
+	foreignId: string;
 }) => {
-    const [clips, setClips] = useState(dummyData);
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [currentMedia, setCurrentMedia] = useState(dummyData[1]);
+	const [clips, setClips] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+	// const [clips, setClips] = useState(dummyData);
+	const [isPlaying, setIsPlaying] = useState(false);
+	const [currentMedia, setCurrentMedia] = useState({});
+	console.log(currentMedia);
 
-    const mediaElement = useRef<any>();
+	const mediaElement = useRef<any>();
 
-    useEffect(() => {
-        if (isPlaying) {
-            mediaElement.current?.play();
-        } else {
-            mediaElement?.current?.pause();
-        }
-    }, [isPlaying]);
+	useEffect(() => {
+		const getData = async () => {
+			try {
+				const response = await fetch(
+					"http://localhost:8080/api/v1/media"
+				);
+				if (!response.ok) {
+					throw new Error(
+						`This is a HTTP error with a status of ${response.status}`
+					);
+				}
+				const data = await response.json();
+				setClips(data);
+				setError(null);
+			} catch (error: any) {
+				setError(error.message);
+				setClips([]);
+			} finally {
+				setLoading(false);
+			}
+		};
+		getData();
+	}, []);
 
-    const onPlaying = () => {
-        const duration = mediaElement.current?.duration;
-        const currentTime = mediaElement.current?.currentTime;
+	useEffect(() => {
+		if (isPlaying) {
+			mediaElement.current?.play();
+		} else {
+			mediaElement?.current?.pause();
+		}
+	}, [isPlaying]);
 
-        setCurrentMedia({
-            ...currentMedia,
-            progress: (currentTime / duration) * 100,
-            length: duration,
-        });
-    };
+	const onPlaying = () => {
+		const duration = mediaElement.current?.duration;
+		const currentTime = mediaElement.current?.currentTime;
 
-    return (
-        <>
-            <li key={id} className={`${styles.item} card`}>
-                <div className="card-content">
-                    {!currentMedia.mediaUrl.includes("mp4") ? (
-                        <div className={`card-image ${styles.imageContainer}`}>
-                            <figure className="image is-4by3">
-                                <img
-                                    src={
-                                        "https://bulma.io/images/placeholders/1280x960.png"
-                                    }
-                                    alt={title}
-                                />
-                            </figure>
-                        </div>
-                    ) : null}
-                    <div className={`${styles.contentContainer} content`}>
-                        {currentMedia.mediaUrl.includes("mp4") ? (
-                            <video
-                                src={currentMedia.mediaUrl}
-                                ref={mediaElement}
-                                onTimeUpdate={onPlaying}
-                            >
-                                {/* <source
-                                src={`${baseUrl}/${foreignId}`}
-                                onTimeUpdate={onPlaying}
-                            /> */}
-                                Your browser does not support the{" "}
-                                <code>audio</code> element.
-                            </video>
-                        ) : (
-                            <audio
-                                src={currentMedia.mediaUrl}
-                                ref={mediaElement}
-                                onTimeUpdate={onPlaying}
-                            >
-                                {/* <source
-                                src={`${baseUrl}/${foreignId}`}
-                                onTimeUpdate={onPlaying}
-                            /> */}
-                                Your browser does not support the{" "}
-                                <code>audio</code> element.
-                            </audio>
-                        )}
-                        <div className="controls">
-                            <Player
-                                mediaElement={mediaElement}
-                                clips={clips}
-                                setClips={setClips}
-                                isPlaying={isPlaying}
-                                setIsPlaying={setIsPlaying}
-                                currentMedia={currentMedia}
-                                setCurrentMedia={setCurrentMedia}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </li>
-        </>
-    );
+		setCurrentMedia({
+			// ...currentMedia,
+			// @ts-ignore
+			progress: (currentTime / duration) * 100,
+			length: duration,
+		});
+	};
+
+	return (
+		<>
+			<li key={id} className={`${styles.item} card`}>
+				<div className="card-content">
+					{/* {currentMedia && !currentMedia.mediaUrl.includes("mp4") ? ( */}
+					<div className={`card-image ${styles.imageContainer}`}>
+						<figure className="image is-4by3">
+							<img
+								src={
+									"https://bulma.io/images/placeholders/1280x960.png"
+								}
+								alt={title}
+							/>
+						</figure>
+					</div>
+					{/* ) : null} */}
+					<div className={`${styles.contentContainer} content`}>
+						{/* {currentMedia.mediaUrl.includes("mp4") ? ( */}
+						<video id="media-file">
+							<source src={`${baseUrl}/${foreignId}`} />
+							Your browser does not support the <code>
+								audio
+							</code>{" "}
+							element.
+						</video>
+						{/* <video
+						src={currentMedia.mediaUrl}
+						ref={mediaElement}
+						onTimeUpdate={onPlaying}
+						>
+							<source
+								src={`${baseUrl}/${foreignId}`}
+								onTimeUpdate={onPlaying}
+							/>
+							Your browser does not support the <code>audio</code>{" "}
+							element.
+						</video> */}
+						) : (
+						<audio
+						// src={currentMedia.mediaUrl}
+						// ref={mediaElement}
+						// onTimeUpdate={onPlaying}
+						>
+							<source src={`${baseUrl}/${foreignId}`} />
+							Your browser does not support the <code>
+								audio
+							</code>{" "}
+							element.
+						</audio>
+						{/* )} */}
+						<div className="controls">
+							{currentMedia && (
+								<Player
+									mediaElement={mediaElement}
+									clips={clips}
+									setClips={setClips}
+									isPlaying={isPlaying}
+									setIsPlaying={setIsPlaying}
+									// @ts-ignore
+									currentMedia={currentMedia}
+									setCurrentMedia={setCurrentMedia}
+								/>
+							)}
+						</div>
+					</div>
+				</div>
+			</li>
+		</>
+	);
 };
 
 export default Media;
--- mclient/src/components/media/player/Player.tsx
+++ mclient/src/components/media/player/Player.tsx
@@ -3,133 +3,133 @@ import Button from "../../shared/Button";
 import styles from "../media.module.scss";
 
 type ClipProps = {
-    id: number;
-    mediaTitle: string;
+	id: number;
+	mediaTitle: string;
 };
 
 export type PlayerProps = {
-    isPlaying: boolean;
-    setIsPlaying: (isPlaying: boolean) => void;
-    currentMedia: {
-        length: any;
-        mediaTitle: string;
-        progress: number;
-    };
-    setCurrentMedia: any;
-    clips: ClipProps[];
-    setClips: any;
-    mediaElement: any;
+	isPlaying: boolean;
+	setIsPlaying: (isPlaying: boolean) => void;
+	currentMedia: {
+		// length: any;
+		mediaTitle: string;
+		progress: number;
+	};
+	setCurrentMedia: any;
+	clips: ClipProps[];
+	setClips: any;
+	mediaElement: any;
 };
 
 const Player = ({
-    isPlaying,
-    setIsPlaying,
-    currentMedia,
-    setCurrentMedia,
-    clips,
-    mediaElement,
+	isPlaying,
+	setIsPlaying,
+	currentMedia,
+	setCurrentMedia,
+	clips,
+	mediaElement,
 }: PlayerProps) => {
-    const playRef = React.useRef<any>();
+	const playRef = React.useRef<any>();
 
-    const startTime = "00:00";
-    const endTime =
-        currentMedia.length && currentMedia.length > 0
-            ? Math.floor(currentMedia.length / 60) +
-              ":" +
-              Math.floor(currentMedia.length % 60)
-            : "00:00";
+	const startTime = "00:00";
+	const endTime = "00:00";
+	// currentMedia.length && currentMedia.length > 0
+	//     ? Math.floor(currentMedia.length / 60) +
+	//       ":" +
+	//       Math.floor(currentMedia.length % 60)
+	//     : "00:00";
 
-    const handlePlayPause = () => {
-        setIsPlaying(!isPlaying);
-    };
+	const handlePlayPause = () => {
+		setIsPlaying(!isPlaying);
+	};
 
-    const checkProgress = (e: any) => {
-        let width = playRef.current?.clientWidth;
-        const offset = e.nativeEvent.offsetX;
+	const checkProgress = (e: any) => {
+		let width = playRef.current?.clientWidth;
+		const offset = e.nativeEvent.offsetX;
 
-        const divProgress = (offset / width) * 100;
-        mediaElement.current.currentTime =
-            (divProgress / 100) * currentMedia.length;
-    };
+		const divProgress = (offset / width) * 100;
+		// mediaElement.current.currentTime =
+		//     (divProgress / 100) * currentMedia.length;
+	};
 
-    const skipToPrevious = () => {
-        const index = clips.findIndex(
-            (item) => item.mediaTitle === currentMedia.mediaTitle
-        );
+	const skipToPrevious = () => {
+		const index = clips.findIndex(
+			(item) => item.mediaTitle === currentMedia.mediaTitle
+		);
 
-        if (index === 0) {
-            setCurrentMedia(clips[clips.length - 1]);
-        } else {
-            setCurrentMedia(clips[index - 1]);
-        }
+		if (index === 0) {
+			setCurrentMedia(clips[clips.length - 1]);
+		} else {
+			setCurrentMedia(clips[index - 1]);
+		}
 
-        mediaElement.currentTime = 0;
-    };
+		mediaElement.currentTime = 0;
+	};
 
-    const skipToNext = () => {
-        const index = clips.findIndex(
-            (item) => item.mediaTitle === currentMedia.mediaTitle
-        );
+	const skipToNext = () => {
+		const index = clips.findIndex(
+			(item) => item.mediaTitle === currentMedia.mediaTitle
+		);
 
-        if (index === clips.length - 1) {
-            setCurrentMedia(clips[0]);
-        } else {
-            setCurrentMedia(clips[index + 1]);
-        }
+		if (index === clips.length - 1) {
+			setCurrentMedia(clips[0]);
+		} else {
+			setCurrentMedia(clips[index + 1]);
+		}
 
-        mediaElement.currentTime = 0;
-    };
+		mediaElement.currentTime = 0;
+	};
 
-    return (
-        <div className={styles.playerContainer}>
-            <div className={styles.title}>
-                <p className={`title is-4 is-size-6-mobile ${styles.title}`}>
-                    {currentMedia.mediaTitle}
-                </p>
-            </div>
-            <div className={styles.progress}>
-                <div className={styles.timeStamp}>
-                    <p>{startTime}</p>
-                    <p>{endTime}</p>
-                </div>
-                <div
-                    className={styles.progressWrapper}
-                    ref={playRef}
-                    onClick={checkProgress}
-                >
-                    <div
-                        className={styles.progressBar}
-                        style={{ width: `${currentMedia.progress + "%"}` }}
-                    ></div>
-                </div>
-            </div>
-            <div className={styles.controls}>
-                <Button
-                    btnClass={`previous`}
-                    children={<i className="fas fa-caret-left"></i>}
-                    onButtonClick={skipToPrevious}
-                />
-                {isPlaying ? (
-                    <Button
-                        btnClass={`pause`}
-                        children={<i className="fas fa-pause"></i>}
-                        onButtonClick={handlePlayPause}
-                    />
-                ) : (
-                    <Button
-                        btnClass={`play`}
-                        children={<i className="fas fa-play"></i>}
-                        onButtonClick={handlePlayPause}
-                    />
-                )}
-                <Button
-                    btnClass={`next`}
-                    children={<i className="fas fa-caret-right"></i>}
-                    onButtonClick={skipToNext}
-                />
-            </div>
-        </div>
-    );
+	return (
+		<div className={styles.playerContainer}>
+			<div className={styles.title}>
+				<p className={`title is-4 is-size-6-mobile ${styles.title}`}>
+					{currentMedia.mediaTitle}
+				</p>
+			</div>
+			<div className={styles.progress}>
+				<div className={styles.timeStamp}>
+					<p>{startTime}</p>
+					<p>{endTime}</p>
+				</div>
+				<div
+					className={styles.progressWrapper}
+					ref={playRef}
+					onClick={checkProgress}
+				>
+					<div
+						className={styles.progressBar}
+						style={{ width: `${currentMedia.progress + "%"}` }}
+					></div>
+				</div>
+			</div>
+			<div className={styles.controls}>
+				<Button
+					btnClass={`previous`}
+					children={<i className="fas fa-caret-left"></i>}
+					onButtonClick={skipToPrevious}
+				/>
+				{isPlaying ? (
+					<Button
+						btnClass={`pause`}
+						children={<i className="fas fa-pause"></i>}
+						onButtonClick={handlePlayPause}
+					/>
+				) : (
+					<Button
+						btnClass={`play`}
+						children={<i className="fas fa-play"></i>}
+						onButtonClick={handlePlayPause}
+					/>
+				)}
+				<Button
+					btnClass={`next`}
+					children={<i className="fas fa-caret-right"></i>}
+					onButtonClick={skipToNext}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Player;
