c5c20311d186aa0d1847f8bb49583add8c1281b8
shirley wang <shirleywang527@gmail.com>
2022-09-08 14:31:57 +0200

    modify show-more button css

--- mclient/src/components/header/Header.tsx
+++ mclient/src/components/header/Header.tsx
@@ -4,58 +4,58 @@ import { ShowMore } from "./ShowMore";
 import styles from "./header.module.scss";
 
 export const Header = () => {
-	const [inputTexts, setInputTexts] = React.useState<string>("");
+    const [inputTexts, setInputTexts] = React.useState<string>("");
 
-	const handleKeyDown = (e: ChangeEvent<HTMLInputElement>) => {
-		console.log(e.target.value);
-	};
+    const handleKeyDown = (e: ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.value);
+    };
 
-	const [isHidden, setIsHidden] = React.useState<boolean>(true);
+    const [isHidden, setIsHidden] = React.useState<boolean>(true);
 
-	const toggleSearchbar = () => {
-		setIsHidden(!isHidden);
-	};
+    const toggleSearchbar = () => {
+        setIsHidden(!isHidden);
+    };
 
-	const ref = React.useRef<HTMLDivElement>(null);
-	const [isActive, setIsActive] = React.useState<boolean>(false);
+    const ref = React.useRef<HTMLDivElement>(null);
+    const [isActive, setIsActive] = React.useState<boolean>(false);
 
-	React.useEffect(() => {
-		const checkIfClickedOutside = (e: MouseEvent) => {
-			if (
-				isActive &&
-				ref.current &&
-				!ref.current.contains(e.target as Node)
-			) {
-				setIsActive(false);
-			}
-		};
+    React.useEffect(() => {
+        const checkIfClickedOutside = (e: MouseEvent) => {
+            if (
+                isActive &&
+                ref.current &&
+                !ref.current.contains(e.target as Node)
+            ) {
+                setIsActive(false);
+            }
+        };
 
-		document.addEventListener("mousedown", checkIfClickedOutside);
+        document.addEventListener("mousedown", checkIfClickedOutside);
 
-		return () => {
-			document.removeEventListener("mousedown", checkIfClickedOutside);
-		};
-	}, [isActive]);
+        return () => {
+            document.removeEventListener("mousedown", checkIfClickedOutside);
+        };
+    }, [isActive]);
 
-	return (
-		<header className={`${styles.header} columns`}>
-			{/* <div data-testid="back-btn" className="icon">
+    return (
+        <header className={`${styles.header}`}>
+            {/* <div data-testid="back-btn" className="icon">
                 <BackIcon />
             </div> */}
 
-			<Search
-				hidden={isHidden}
-				onClick={toggleSearchbar}
-				value={inputTexts}
-				setKeywords={setInputTexts}
-				onKeyDown={handleKeyDown}
-			/>
+            <Search
+                hidden={isHidden}
+                onClick={toggleSearchbar}
+                value={inputTexts}
+                setKeywords={setInputTexts}
+                onKeyDown={handleKeyDown}
+            />
 
-			<ShowMore
-				dropdownRef={ref}
-				state={isActive}
-				setState={setIsActive}
-			/>
-		</header>
-	);
+            <ShowMore
+                dropdownRef={ref}
+                state={isActive}
+                setState={setIsActive}
+            />
+        </header>
+    );
 };
