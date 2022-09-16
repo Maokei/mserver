2d4280bf8927b5d6fb116be822acf6f97b0f2aab
shirley <shirleywang527@gmail.com>
2022-09-16 13:46:52 +0200

    commit update

--- mclient/src/components/home/Home.tsx
+++ mclient/src/components/home/Home.tsx
@@ -11,71 +11,67 @@ import styles from "./home.module.scss";
 // };
 
 const Home = () => {
-    const { data, loading, error } = useMediaData();
+	const { data, loading, error } = useMediaData();
 
-    return (
-        <>
-            <main className={styles.container}>
-                <aside className={styles.menu}>
-                    <ul className="menuTop">
-                        <li>
-                            <a href="/">Home</a>
-                        </li>
-                        <li>
-                            <a href="/library">Library</a>
-                        </li>
-                    </ul>
+	return (
+		<>
+			<main className={styles.container}>
+				<aside className={styles.menu}>
+					<ul className="menuTop">
+						<li>
+							<a href="/">Home</a>
+						</li>
+						<li>
+							<a href="/library">Library</a>
+						</li>
+					</ul>
 
-                    <div className={styles.divider} />
+					<div className={styles.divider} />
 
-                    <ul className="menuBottom">
-                        <li>
-                            <a href="/beats">Beats</a>
-                        </li>
-                        <li>
-                            <a href="/ambient">Ambient</a>
-                        </li>
-                        <li>
-                            <a href="/synthetic">Synthetic</a>
-                        </li>
-                    </ul>
-                </aside>
+					<ul className="menuBottom">
+						<li>
+							<a href="/music">Music</a>
+						</li>
+						<li>
+							<a href="/video">Video</a>
+						</li>
+						<li>
+							<a href="/podcast">Podcast</a>
+						</li>
+					</ul>
+				</aside>
 
-                <div className={styles.recentlyPlayed}>
-                    <p className="title is-4 is-size-6-mobile">
-                        Recently Played
-                    </p>
+				<div className={styles.recentlyPlayed}>
+					<p className="title is-4 is-size-6-mobile">
+						Recently Played
+					</p>
 
-                    {error && <p>{error}</p>}
-                    {loading ? (
-                        <p>Loading ...</p>
-                    ) : (
-                        <div className={styles.cardsGroup}>
-                            {data &&
-                                data.map((item) => (
-                                    <Card key={item.id} {...item} />
-                                ))}
-                        </div>
-                    )}
-                </div>
-            </main>
+					{error && <p>{error}</p>}
+					{loading ? (
+						<p>Loading ...</p>
+					) : (
+						<div className={styles.cardsGroup}>
+							{data &&
+								data.map((item) => (
+									<Card key={item.id} {...item} />
+								))}
+						</div>
+					)}
+				</div>
+			</main>
 
-            <Player
-                id={""}
-                isFull={false}
-                setId={function (e: string): void {
-                    throw new Error("Function not implemented.");
-                }}
-                setIsFull={function (e: boolean): void {
-                    throw new Error("Function not implemented.");
-                }}
-            />
-            {/* <div className={styles.playingNow}>
-                <MediaProgress />
-                <MediaPlaying />
-            </div> */}
-        </>
-    );
+			<Player
+				id={""}
+				isFull={false}
+				setId={function (e: string): void {
+					throw new Error("Function not implemented.");
+				}}
+				setIsFull={function (e: boolean): void {
+					throw new Error("Function not implemented.");
+				}}
+			/>
+		</>
+	);
 };
 
 export default Home;
