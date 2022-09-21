49771fd6eb79b7c35c339e25d979167009aa1c07
shirley <shirleywang527@gmail.com>
2022-09-21 09:20:38 +0200

    modify pllayer

--- mclient/src/components/shared/Card.tsx
+++ mclient/src/components/shared/Card.tsx
@@ -2,21 +2,33 @@ import styles from "../home/home.module.scss";
 import { ItemProps } from "../../types";
 
 export const Card = ({ id, title, setId, mediaId, media }: ItemProps) => {
-    return (
-        <div className={`${styles.card} card`} onClick={() => setId(mediaId)}>
-            <div className="card-image">
-                <figure className="image is-1by1">
-                    <img
-                        className="is-rounded"
-                        src={"https://bulma.io/images/placeholders/128x128.png"}
-                        alt={title}
-                    />
-                </figure>
-                <div className={`${styles.album} media-content`}>
-                    <p className="title is-4">{title}</p>
-                </div>
-                <audio src={media} />
-            </div>
-        </div>
-    );
+	return (
+		<>
+			{mediaId === id && (
+				<div
+					className={`${styles.card} card`}
+					onClick={() => {
+						console.log(id);
+						setId(mediaId);
+					}}
+				>
+					<div className="card-image">
+						<figure className="image is-1by1">
+							<img
+								className="is-rounded"
+								src={
+									"https://bulma.io/images/placeholders/128x128.png"
+								}
+								alt={title}
+							/>
+						</figure>
+						<div className={`${styles.album} media-content`}>
+							<p className="title is-4">{title}</p>
+						</div>
+						<audio src={media} />
+					</div>
+				</div>
+			)}
+		</>
+	);
 };
