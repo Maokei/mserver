9d079be8271a26881902402bb16fbd815561427c
maokei <rickard.joh@gmail.com>
2022-09-24 20:41:53 +0200

    playlist

--- mserver/src/main/java/se/maokei/mserver/model/Playlist.java
+++ mserver/src/main/java/se/maokei/mserver/model/Playlist.java
@@ -1,4 +1,15 @@
 package se.maokei.mserver.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Playlist extends EntityMetadata {
+    private String playlistName;
+    private List<Media> mediaList;
 }
