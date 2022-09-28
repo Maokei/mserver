eb6c40a5d99ca6ccf39eff9634dec262bdade4cc
shirley wang <shirleywang527@gmail.com>
2022-09-28 15:28:33 +0200

    add barebone youtube-dl

--- mclient/src/components/home/Home.tsx
+++ mclient/src/components/home/Home.tsx
@@ -1,71 +1,90 @@
 import React from "react";
 import { useMediaData } from "../../context/mediaDataContext";
+import useModal from "../../hooks/useModal";
+import Modal from "../modal/Modal";
 import Player from "../player/Player";
 import { Card } from "../shared/Card";
 import styles from "./home.module.scss";
 
 const Home = () => {
-	const { data, loading, error } = useMediaData();
+    const { data, loading, error } = useMediaData();
+    const { isShowing, toggle } = useModal();
 
-	const [id, setId] = React.useState<string>("");
+    const [id, setId] = React.useState<string>("");
 
-	return (
-		<>
-			<main className={styles.container}>
-				<aside className={styles.menu}>
-					<ul className="menuTop">
-						<li>
-							<a href="/">Home</a>
-						</li>
-						<li>
-							<a href="/library">Library</a>
-						</li>
-					</ul>
+    return (
+        <>
+            <main className={styles.container}>
+                <aside className={styles.menu}>
+                    <ul className="menuTop">
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/library">Library</a>
+                        </li>
+                        <li>
+                            <button
+                                className={styles.downloadButton}
+                                onClick={toggle}
+                            >
+                                Download
+                            </button>
+                            <Modal
+                                isShowing={isShowing}
+                                hide={toggle}
+                                modalName="Download Youtube clip"
+                                labelText={["URL"]}
+                                fileType="Audio"
+                                isAudio={true}
+                            />
+                        </li>
+                    </ul>
 
-					<div className={styles.divider} />
+                    <div className={styles.divider} />
 
-					<ul className="menuBottom">
-						<li>
-							<a href="/music">Music</a>
-						</li>
-						<li>
-							<a href="/video">Video</a>
-						</li>
-						<li>
-							<a href="/podcast">Podcast</a>
-						</li>
-					</ul>
-				</aside>
+                    <ul className="menuBottom">
+                        <li>
+                            <a href="/music">Music</a>
+                        </li>
+                        <li>
+                            <a href="/video">Video</a>
+                        </li>
+                        <li>
+                            <a href="/podcast">Podcast</a>
+                        </li>
+                    </ul>
+                </aside>
 
-				<div className={styles.recentlyPlayed}>
-					<p className="title is-4 is-size-6-mobile">
-						Recently Played
-					</p>
+                <div className={styles.recentlyPlayed}>
+                    <p className="title is-4 is-size-6-mobile">
+                        Recently Played
+                    </p>
 
-					{error && <p>{error}</p>}
-					{loading ? (
-						<p>Loading ...</p>
-					) : (
-						<div className={styles.cardsGroup}>
-							{data &&
-								data.map((item) => (
-									<Card
-										key={item.id}
-										id={item.id}
-										setId={setId}
-										title={item.title}
-										mediaId={item.id}
-										media={`http://localhost:8080/api/v1/media/${item.foreignId}`}
-									/>
-								))}
-						</div>
-					)}
-				</div>
-			</main>
+                    {error && <p>{error}</p>}
+                    {loading ? (
+                        <p>Loading ...</p>
+                    ) : (
+                        <div className={styles.cardsGroup}>
+                            {data &&
+                                data.map((item) => (
+                                    <Card
+                                        key={item.id}
+                                        id={item.id}
+                                        setId={setId}
+                                        title={item.title}
+                                        mediaId={item.id}
+                                        media={`http://localhost:8080/api/v1/media/${item.foreignId}`}
+                                    />
+                                ))}
+                        </div>
+                    )}
+                </div>
+            </main>
 
-			<Player id={id} setId={setId} />
-		</>
-	);
+            <Player id={id} setId={setId} />
+        </>
+    );
 };
 
 export default Home;
--- mclient/src/types.ts
+++ mclient/src/types.ts
@@ -11,3 +11,13 @@ export type CardProps = {
     id: string;
     item: ItemProps;
 };
+
+export type ModalProps = {
+    isShowing: boolean;
+    hide: () => void;
+    modalName: string;
+    labelText: string[];
+    isAudio?: boolean;
+    isVideo?: boolean;
+    fileType?: string;
+};
