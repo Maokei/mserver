2afe9e44f1962a7cb11de618030e6c301f5d7f81
maokei <rickard.joh@gmail.com>
2022-10-03 15:36:32 +0200

    c

--- mserver/src/main/java/se/maokei/mserver/services/CommentService.java
+++ mserver/src/main/java/se/maokei/mserver/services/CommentService.java
@@ -1,7 +1,23 @@
 package se.maokei.mserver.services;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
+@AllArgsConstructor
 @Service
 public class CommentService {
+    public void createComment() {
+
+    }
+
+    public void readComment() {
+        
+    }
+
+    public void updateComment() {
+
+    }
+
+    public void deleteComment() {
+
+    }
 }
