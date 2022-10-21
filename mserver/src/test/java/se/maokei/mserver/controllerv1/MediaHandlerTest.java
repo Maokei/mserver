7bfb0b4e4d879f99214956a5b86a4ee5c05479a9
maokei <rickard.joh@gmail.com>
2022-10-22 00:22:59 +0200

    Controller tests

---
+++ mserver/src/test/java/se/maokei/mserver/controllerv1/MediaHandlerTest.java
@@ -0,0 +1,7 @@
+package se.maokei.mserver.controllerv1;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MediaHandlerTest {
+}
---
+++ mserver/src/test/java/se/maokei/mserver/controllerv1/RegisterControllerTest.java
@@ -0,0 +1,19 @@
+package se.maokei.mserver.controllerv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+public class RegisterControllerTest {
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    public void registerNewUser() {
+
+    }
+}
