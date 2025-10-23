f70ad6ed72f57bd706bbaf3bc41d261ef993abce
maokei <rickard.joh@gmail.com>
2025-10-23 23:02:01 +0900

    pkgs

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,13 +1,21 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const navigate = useNavigate();
   const [dropDown, setDropDownState] = useState(false);
   const toggleDropDown = () => setDropDownState(!dropDown);
+
+  const handleClick = () => {
+    navigate('/');
+  };
 
   return (
     <header className='w-full bg-white shadow p-4 sticky top-0 z-10'>
       <nav className='flex justify-between items-center'>
-        <div className='text-2xl font-bold'>MServer</div>
+        <div className='text-2xl font-bold' onClick={handleClick}>
+          MServer
+        </div>
         <button onClick={toggleDropDown}>
           <span className='hidden md:block font-medium'>User</span>
         </button>
